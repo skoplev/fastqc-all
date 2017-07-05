c1e0a0c993302780f11b95013bdefcfad54a7cf9
skoplev <skoplev@gmail.com>
2017-07-05 07:57:13 -0400

    LSF SAM to BAM conversion.

--- tuxedo2/parSAM2BAM.sh
+++ tuxedo2/parSAM2BAM.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#BSUB -J samtools
+#BSUB -P acc_STARNET
+#BSUB -q alloc
+#BSUB -W 12:00
+#BSUB -M 8000
+#BSUB -n 1
+#BSUB -e logs/error.%J
+#BSUB -o logs/output.%J
+
+# USAGE:
+# bsub -cwd <dir> < tuxedo2/parSAM2BAM.sh
 
 module load samtools
 
 # Convert SAM to BAM files, also sorts based on genomic coordinates
-cd align
 mkdir bam
 for file in *.sam; do
 	echo "Converting $file"
@@ -13,5 +22,4 @@ for file in *.sam; do
 done
 
 # Cleanup sam files, which can be very large and contain the same information as the sorted bam files
-rm *.sam
-cd ..
+# rm *.sam

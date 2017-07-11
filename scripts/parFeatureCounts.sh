0c64d0c85dd3873db4007c7021d82a496c7ea6c8
skoplev <skoplev@gmail.com>
2017-07-11 15:22:04 -0400

    Subread feature counts.

---
+++ scripts/parFeatureCounts.sh
@@ -0,0 +1,22 @@
+#BSUB -J featureCounts
+#BSUB -P acc_STARNET
+#BSUB -q alloc
+#BSUB -W 12:00
+#BSUB -R "rusage[mem=2000]"
+#BSUB -M 2000
+#BSUB -n 4
+#BSUB -e logs/error.%J
+#BSUB -o logs/output.%J
+
+module load subread
+
+annot=~/links/STARNET/koples01/data_bases/HumanGenome/ensemble_annot/Homo_sapiens.GRCh38.89.gtf
+
+mkdir feature_counts
+
+featureCounts -T 8 \
+	-t exon \
+	-g gene_id \
+	-a $annot \
+	-o feature_counts/gene_counts.txt \
+	*.bam

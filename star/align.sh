ef0dd35b19b453600b2ebec11916113e7aa3c4d9
Simon Koplev <koples01@minerva4.mgmt.hpc.mssm.edu>
2018-02-14 23:58:54 -0500

    Parallel LSF job submit of STAR alignments.

--- star/align.sh
+++ star/align.sh
@@ -1,11 +1,16 @@
-# Align 
+# Align RNA-seq using STAR
+# Loops over fastq.gz files specified in text file, submitting a STAR
+# alignemnt LSF job for each fastq.gz file.
 
 # USAGE:
-# star/align.sh 
+# star/aligh.sh
+
+module load star
 
 mkdir logs
 
-fastq_list="/sc/orga/projects/STARNET/koples01/case-control-align/test_fastq_files.txt"
+# fastq_list="/sc/orga/projects/STARNET/koples01/case-control-align/test_fastq_files.txt"
+fastq_list="/sc/orga/projects/STARNET/koples01/case-control-align/fastq_files.txt"
 
 fastq_files=`cat $fastq_list`
 
@@ -18,8 +23,7 @@ genome="/sc/orga/projects/STARNET/koples01/case-control-align/genome"
 
 ucscGTF="/sc/orga/projects/STARNET/vamsi/ucscgtf/ucsc.gtf"
 
-
-module load star
+mkdir align
 
 # for file in *.fastq; do
 # cat fastq_list | while read line; do
@@ -28,26 +32,26 @@ for file in $fastq_files; do
 
 	filename=$(basename $file)
 
-	#BSUB -J STAR_"${filename}"
-	#BSUB -P acc_STARNET
-	#BSUB -q alloc
-	#BSUB -W 6:00
-	#BSUB -R "rusage[mem=4000]"
-	#BSUB -n 8
-	#BSUB -e logs/error.%J
-	#BSUB -o logs/output.%J
-
-	STAR --genomeDir $genome \
-		--sjdbGTFfile $stargtf \
-		--readFilesIn $file \
-		--readFilesCommand zcat \
-		--runThreadN 24 \
-		--outReadsUnmapped Fastx \
-		--chimSegmentMin 15 \
-		--chimJunctionOverhangMin 15 \
-		--outSAMstrandField intronMotif \
-		--outStd BAM_SortedByCoordinate \
-		--outSAMtype BAM \
-		SortedByCoordinate >alignments.bam
+	bsub -J STAR \
+		-P acc_STARNET \
+		-q alloc \
+		-W 6:00 \
+		-R "rusage[mem=6000]" \
+		-M 6000 \
+		-n 8 \
+		-e logs/error.%J \
+		-o logs/output.%J \
+		-R "span[hosts=1]" \
+		STAR --genomeDir $genome \
+			--sjdbGTFfile $stargtf \
+			--readFilesIn $file \
+			--readFilesCommand zcat \
+			--runThreadN 16 \
+			--outReadsUnmapped Fastx \
+			--chimSegmentMin 15 \
+			--chimJunctionOverhangMin 15 \
+			--outSAMstrandField intronMotif \
+			--outSAMtype BAM SortedByCoordinate \
+			--outFileNamePrefix align/$filename.
 done
 
